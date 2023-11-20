deb09d10a17e952d60a2e88f855cd44550c7ab06
hellory4n <hellory4n@gmail.com>
2023-11-20 16:26:31 -0400

    how the fuck is it null

--- writer/writer.gd
+++ writer/writer.gd
@@ -1,6 +1,9 @@
 extends TextEdit
 
-@export var the_thing: RichTextLabel
+@export var the_thing: Control
 
-func _on_text_changed():
-	the_thing.push_bold()
+func _on_tab_container_tab_changed(tab: int):
+	if tab == 1:
+		the_thing.add_child(RichTextLabel.new())
+	else:
+		the_thing.get_node("yes").queue_free()
