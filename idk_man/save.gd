132d9e7c64426a7076c049a8d367df74ac70a804
hellory4n <hellory4n@gmail.com>
2023-11-23 12:33:44 -0500

    i'm gonna rewrite this in c# lol

--- idk_man/save.gd
+++ idk_man/save.gd
@@ -1,19 +1,19 @@
 extends Button
 
-@export var encyclopedia: Control
-@onready var tree: EncyclopediaTree = encyclopedia.get_node("SidebarThingy/fjnjg/Tree")
-var awesome_tree_thingy = {}
-var already_processed = []
-
-func _pressed():
-	awesome_tree_thingy = add_thingy(tree.items)
-
-func add_thingy(dict: Dictionary, add_to_parent = false) -> Dictionary:
-	awesome_tree_thingy.merge({
-		name = ky.get_text(0),
-		content = value.get_node("TabContainer/Writer/Gkggg").text,
-		children = []
-	})
-	
-	for child in key.get_children():
-		
+#@export var encyclopedia: Control
+#@onready var tree: EncyclopediaTree = encyclopedia.get_node("SidebarThingy/fjnjg/Tree")
+#var awesome_tree_thingy = {}
+#var already_processed = []
+#
+#func _pressed():
+	#awesome_tree_thingy = add_thingy(tree.items)
+#
+#func add_thingy(dict: Dictionary, add_to_parent = false) -> Dictionary:
+	#awesome_tree_thingy.merge({
+		#name = ky.get_text(0),
+		#content = value.get_node("TabContainer/Writer/Gkggg").text,
+		#children = []
+	#})
+	#
+	#for child in key.get_children():
+		#
